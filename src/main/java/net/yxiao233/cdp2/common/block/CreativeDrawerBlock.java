1bd821ef3f019f78ae32f5e6f97e5c248fa2ead0
Y-Xiao233 <1870377449@qq.com>
2026-02-03 14:45:59 +0800

    change creative drawer texture

--- src/main/java/net/yxiao233/cdp2/common/block/CreativeDrawerBlock.java
+++ src/main/java/net/yxiao233/cdp2/common/block/CreativeDrawerBlock.java
@@ -6,10 +6,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.yxiao233.cdp2.api.annotation.AutoRegistryBlockCapabilities;
 import net.yxiao233.cdp2.api.block.CDPTickableEntityBlock;
@@ -27,21 +25,15 @@ import java.util.function.Supplier;
 public class CreativeDrawerBlock extends CDPTickableEntityBlock implements IRotatableBlock{
     private final Supplier<ItemStack> infinityItem;
     private final ResourceLocation entityTypeLocation;
-    public CreativeDrawerBlock(Properties properties, Supplier<ItemStack> infinityItem, ResourceLocation entityTypeLocation) {
-        super(properties.sound(SoundType.WOOD).strength(0.3F));
-        this.infinityItem = infinityItem;
-        this.entityTypeLocation = entityTypeLocation;
-    }
-
     public CreativeDrawerBlock(Supplier<ItemStack> infinityItem, ResourceLocation entityTypeLocation) {
-        super(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).sound(SoundType.WOOD).strength(0.3F));
+        super(Properties.ofFullCopy(Blocks.AMETHYST_BLOCK).strength(0.3F));
         this.infinityItem = infinityItem;
         this.entityTypeLocation = entityTypeLocation;
     }
 
     @Override
     protected @NotNull MapCodec<? extends BaseEntityBlock> codec() {
-        return simpleCodec(properties -> new CreativeDrawerBlock(properties,infinityItem,entityTypeLocation));
+        return simpleCodec(properties -> new CreativeDrawerBlock(infinityItem,entityTypeLocation));
     }
 
     @Override
